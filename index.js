6755aaee4ddc686fc2f77be9e925fec6668ceb35
nasirdiu <nasir.joypurhat@gmail.com>
2022-05-17 16:29:43 +0600

    aa

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ const port = process.env.PORT || 5000;
 const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 //middle Ware
-app.use(cors());
+const corsConfig = {
+  origin: true,
+  credentials: true,
+};
+app.use(cors(corsConfig));
+app.options("*", cors(corsConfig));
+
+// app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nw0lg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
